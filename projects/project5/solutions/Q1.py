4240f18c43dc2e5acdf51fa79df3fe51c808af45
Nicolas Revelli <nicolas.revelli.mc@gmail.com>
2025-02-03 18:25:20 +0100

    terminé les parties 1,2 et 3

--- projects/project5/solutions/Q1.py
+++ projects/project5/solutions/Q1.py
@@ -3,12 +3,12 @@ class ModelWithSingleDropout(nn.Module):
     def __init__(self):
         super(ModelWithSingleDropout, self).__init__()
         self.fc1 = nn.Linear(784, 120)
-        self.dropout = nn.Dropout(p=0.5)  # créer la couche de dropout avec p=0.5
+        self.dropout = nn.Dropout(p=0.5)
         self.fc2 = nn.Linear(120, 10)
 
     def forward(self, x):
         x = x.view(-1, 784)
         x = F.relu(self.fc1(x))
-        x = self.dropout(x)  # Appliquer le dropout après la première couche
+        x = self.dropout(x)  # Appliquer le dropout après la première couche cachée
         x = self.fc2(x)
         return x
--- projects/project5/solutions/Q2.py
+++ projects/project5/solutions/Q2.py
@@ -2,11 +2,11 @@
 class RandomDropoutModel(nn.Module):
     def __init__(self):
         super(RandomDropoutModel, self).__init__()
-        self.dropout1 = nn.Dropout(p=0.2) # Dropout avec une probabilité de 20%
+        self.dropout1 = nn.Dropout(p=0.25) # Dropout avec une probabilité de 25%
         self.fc1 = nn.Linear(784, 120)
-        self.dropout2 = nn.Dropout(p=0.5)  # Dropout avec une probabilité de 50%
+        self.dropout2 = nn.Dropout(p=0.4)  # Dropout avec une probabilité de 40%
         self.fc2 = nn.Linear(120, 60)
-        self.dropout3 = nn.Dropout(p=0.5)  # Dropout avec une probabilité de 50%
+        self.dropout3 = nn.Dropout(p=0.4)  # Dropout avec une probabilité de 40%
         self.fc3 = nn.Linear(60, 10)
 
     def forward(self, x):
