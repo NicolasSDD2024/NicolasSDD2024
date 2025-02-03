cccfe66c1e4cc8f8415c7dae64daa3d1c30247ec
Nicolas Revelli <nicolas.revelli.mc@gmail.com>
2025-02-03 10:05:46 +0100

    Finalisation des premières parties du notebook

--- projects/project5/solutions/Q1.py
+++ projects/project5/solutions/Q1.py
@@ -1,8 +1,9 @@
-class ModelWithDropout(nn.Module):
+# Définition de la classe du modèle avec une couche de Dropout
+class ModelWithSingleDropout(nn.Module):
     def __init__(self):
-        super(ModelWithDropout, self).__init__()
+        super(ModelWithSingleDropout, self).__init__()
         self.fc1 = nn.Linear(784, 120)
-        self.dropout = nn.Dropout(p=0.5)  # La couche de dropout avec p=0.5
+        self.dropout = nn.Dropout(p=0.5)  # créer la couche de dropout avec p=0.5
         self.fc2 = nn.Linear(120, 10)
 
     def forward(self, x):
